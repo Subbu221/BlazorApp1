e7f7dfafb1f038927c20bee2b3120979d779a997
Venkata Subba Reddy Dunnuthula <dvsreddy221@gmail.com>
2021-06-09 17:37:22 +0530

    Radzen Grid

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Radzen;
+
 
 namespace BlazorApp1
 {
@@ -14,9 +16,14 @@ namespace BlazorApp1
     {
         public static async Task Main(string[] args)
         {
+           
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://schoolwebapi.azurewebsites.net/api/") });
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
