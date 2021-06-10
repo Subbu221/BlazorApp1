e7537f2ca3c83ed05d50144ad80c236c676c980e
Venkata Subba Reddy Dunnuthula <dvsreddy221@gmail.com>
2021-06-10 17:55:14 +0530

    GriD CURD

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -24,8 +24,8 @@ namespace BlazorApp1
             builder.Services.AddScoped<NotificationService>();
             builder.Services.AddScoped<TooltipService>();
             builder.Services.AddScoped<ContextMenuService>();
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://schoolwebapi.azurewebsites.net/api/") });
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://schoolwebapi.azurewebsites.net/api/") });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44398/api/") });
             //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
         }
